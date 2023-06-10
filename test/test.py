8bf6d72db9e5eea656ef26fa0db55bcb72f8ef16
Yupei You <youyupei@gmail.com>
2023-06-10 17:13:47 +1000

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ def random_seq_generator():
     while True:
         yield ''.join(np.random.choice(['A', 'G', "C", 'T'],  size=np.random.randint(20,50)))
 rand_seq = random_seq_generator()
-print("Runtime test no cutoff(1_000_000 iteration):")
+print("Runtime test no max ed cutoff(1_000_000 iteration):")
 np.random.seed(2023)
 start_time = time.time()
 a = next(rand_seq)
@@ -18,7 +18,7 @@ l_ed = []
 for i in range(1_000_000):
     l_ed.append(Levenshtein.distance(a, b))
 end_time = time.time()
-print(f"Levenshtein.distance (no cutoff): {end_time - start_time}" )
+print(f"Levenshtein.distance: {end_time - start_time:.2f}s" )
 
 np.random.seed(2023)
 start_time = time.time()
@@ -28,11 +28,11 @@ fast_ed = []
 for i in range(1_000_000):
     fast_ed.append(fast_edit_distance.edit_distance(a, b))
 end_time = time.time()
-print(f"fast_edit_distance.edit_distance (no cutoff): { end_time - start_time}")
-print(" Result consistant?")
+print(f"fast_edit_distance.edit_distance: {end_time - start_time:.2f}s")
+print("Result consistant?")
 print("No!" if any(np.array(l_ed) != np.array(fast_ed)) else "Yes!")
 
-print("Runtime test with cutoff 5 (1_000_000 iteration):")
+print("Runtime test with max ed cutoff 5 (1_000_000 iteration):")
 np.random.seed(2023)
 start_time = time.time()
 a = next(rand_seq)
@@ -41,7 +41,7 @@ l_ed = []
 for i in range(1_000_000):
     l_ed.append(Levenshtein.distance(a, b, score_cutoff=5))
 end_time = time.time()
-print(f"Levenshtein.distance (no cutoff): {end_time - start_time}" )
+print(f"Levenshtein.distance: {end_time - start_time:.2f}s" )
 
 np.random.seed(2023)
 start_time = time.time()
@@ -51,6 +51,6 @@ fast_ed = []
 for i in range(1_000_000):
     fast_ed.append(fast_edit_distance.edit_distance(a, b, 5))
 end_time = time.time()
-print(f"fast_edit_distance.edit_distance (no cutoff): { end_time - start_time}")
-print(" Result consistant?")
+print(f"fast_edit_distance.edit_distance: {end_time - start_time:.2f}s")
+print("Result consistant?")
 print("No!" if any(np.array(l_ed) != np.array(fast_ed)) else "Yes!")
