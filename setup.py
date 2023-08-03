184348d214c67c7275987059601f82fdccfc5a86
youyupei <youyupei@gmail.com>
2023-08-03 15:22:53 +1000

    add github workflow for pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-# from distutils.core import setup
-# from Cython.Build import cythonize
-
-# setup(
-#     name="fast_edit_distance",
-#     version="1.0",
-#     ext_modules=cythonize("src/fast_edit_distance.pyx"),
-# )
-
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+
 setup(
     name='fast_edit_distance',
-    version='1.2.0',
+    version='1.2.1',
     author='Yupei You',
     author_email="youyupei@gmail.com",
     description='Implementation of edit distance calculation.',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/youyupei/fast_edit_distance",
     install_requires=['cython'],
     classifiers=[
