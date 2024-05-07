03188e1422d03f6914d495a0bf3425a829a71d1f
Alexandre Jasmin <ajasmin@esmartcontrol.com>
2024-05-06 22:58:10 -0400

    build: Transition to pyproject.toml
    - Move project metadata from setup.py to pyproject.toml
    - Add the Cython compiler as a build requirement for isolated builds

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from Cython.Build import cythonize
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
-
-
 setup(
-    name='fast_edit_distance',
-    version='1.2.1',
-    author='Yupei You',
-    author_email="youyupei@gmail.com",
-    description='Implementation of edit distance calculation.',
-    packages=find_packages(),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url="https://github.com/youyupei/fast_edit_distance",
-    install_requires=['cython'],
-    classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
     ext_modules=cythonize("src/fast_edit_distance.pyx")
 )
