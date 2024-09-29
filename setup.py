b4a3080a691c5826ebda378a35b30f753a23bf89
youyupei <youyupei@gmail.com>
2024-09-29 15:52:38 +1000

    Fix dynamic versioning
    add fetch-depth to gh workflow
    
    add fetch-depth to gh workflow: v2
    
    Update GHA
    
    Update GHA2
    
    Update GHA3
    
    Update GHA4
    
    Update GHA5
    
    Update GHA6

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import setuptools_scm
 
+version = setuptools_scm.get_version()
 setup(
-    ext_modules=cythonize("src/fast_edit_distance.pyx")
+    ext_modules=cythonize("src/fast_edit_distance.pyx"),
+    version=version # I have to do this because setuptools_scm doesn't work with Cython (the cythonize function would modify the repo so that the version is not the same as the one in the repo)
 )
